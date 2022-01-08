fce236e3ed6fe5e2cd83bc317ee1960d529f6278
Khaled El Hamzi <contact@elhamzi.me>
2022-01-08 17:28:54 +0100

    Apply data annotations to category model.

--- Domain/Category.cs
+++ Domain/Category.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Domain
 {
     internal class Category
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         public int Order { get; set; }  
