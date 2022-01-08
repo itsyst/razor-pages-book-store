6e171f6f273c25f6d3059c90ad09d4c21b533cc3
Khaled El Hamzi <contact@elhamzi.me>
2022-01-08 21:09:08 +0100

    Run migration and update database.

---
+++ Infrastructure/Migrations/20220108193446_AddCategoryToDatabase.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddCategoryToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(55)", maxLength: 55, nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name", "Order" },
+                values: new object[,]
+                {
+                    { 1, "Romance", 1 },
+                    { 2, "Horror", 2 },
+                    { 3, "Finance", 2 },
+                    { 4, "Biography", 3 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
