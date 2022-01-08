400ced9e6a2c2089f375740b9aa22389fd468ac9
Khaled El Hamzi <contact@elhamzi.me>
2022-01-08 21:10:38 +0100

    Add services to startup program file.

--- RazzorPagesBooksApp/Program.cs
+++ RazzorPagesBooksApp/Program.cs
@@ -1,6 +1,17 @@
+using Application.Interfaces;
+using Infrastructure.Persistence;
+using Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
