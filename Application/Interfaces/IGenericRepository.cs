8ae03a02060604735ca2d55d065df0d08984f37f
Khaled El Hamzi <contact@elhamzi.me>
2022-01-08 21:02:34 +0100

    Create application layer - generic repository.

---
+++ Application/Interfaces/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Application.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(object id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(object id);
+    }
+}
