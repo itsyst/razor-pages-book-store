8a887b455b27e4e89f8b324e1f6c78abfabfacbe
Khaled El Hamzi <contact@elhamzi.me>
2022-01-09 06:28:47 +0100

    Run migration and updare database.

---
+++ RazzorPagesBooksApp/Migrations/20220109052435_UpdateTableCategory.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazzorPagesBooksApp.Migrations
+{
+    public partial class UpdateTableCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(55)", maxLength: 55, nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "Id", "Name", "Order" },
+                values: new object[,]
+                {
+                    { 1, "Romance", 1 },
+                    { 2, "Horror", 2 },
+                    { 3, "Finance", 2 },
+                    { 4, "Biography", 3 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Category");
+        }
+    }
+}
