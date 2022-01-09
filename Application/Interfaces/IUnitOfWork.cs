7190b80ba2f3e14f49a5722e8f3f49e2d8e337b2
Khaled El Hamzi <contact@elhamzi.me>
2022-01-09 05:06:33 +0100

    Implement unit of work pattern

---
+++ Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿ 
+
+namespace Application.Interfaces
+{
+    public interface IUnitOfWork<T> where T : class
+    {
+        IGenericRepository<T> Entity { get; }
+        void Save();
+    }
+}
---
+++ Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using Application.Interfaces;
+using Infrastructure.Persistence;
+using Infrastructure.Repository;
+
+namespace Infrastructure.UnitOfWork
+{
+    public class UnitOfWork<T> : IUnitOfWork<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private IGenericRepository<T> _entity;
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IGenericRepository<T> Entity
+        {
+            get
+            {
+                return _entity ?? (_entity = new GenericRepository<T>(_context));
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
