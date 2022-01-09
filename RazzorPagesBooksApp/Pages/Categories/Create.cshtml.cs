2e521b3fe3b6605ccdbec653bea7ac74b1b06960
Khaled El Hamzi <contact@elhamzi.me>
2022-01-09 05:57:58 +0100

    Implement the onpost method.

--- RazzorPagesBooksApp/Pages/Categories/Create.cshtml.cs
+++ RazzorPagesBooksApp/Pages/Categories/Create.cshtml.cs
@@ -1,15 +1,36 @@
+using Application.Interfaces;
 using Domain;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazzorPagesBooksApp.Pages.Categories
 {
+    [BindProperties]
     public class CreateModel : PageModel
     {
+        private readonly IUnitOfWork<Category> _category;
+
+        public CreateModel(IUnitOfWork<Category> category)
+        {
+            _category = category;
+        }
+
         public Category  Category { get; set; }
 
         public void OnGet()
         {
+        }
 
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _category.Entity.Insert(Category);
+            _category.Save();
+            return RedirectToPage("Index");
         }
     }
 }
